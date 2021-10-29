84517da3018b196e1a12c381b82bee69363f0111
Nimanita <45235857+Nimanita@users.noreply.github.com>
2021-10-29 18:33:19 +0530

    Create cd.php

---
+++ src/cd.php
@@ -0,0 +1,28 @@
+project_urls={
+        "Documentation": "https://tweepy.readthedocs.io",
+        "Issue Tracker": "https://github.com/tweepy/tweepy/issues",
+        "Source Code": "https://github.com/tweepy/tweepy",
+    },
+    download_url="https://pypi.org/project/tweepy/",
+    packages=find_packages(exclude=["tests", "examples"]),
+   self.api_codes.append(error["code"])
+                if "message" in error:
+                    self.api_messages.append(error["message"])
+                if "code" in error and "message" in error:
+                    error_text += f"\n{error['code']} - {error['message']}"
+                elif "message" in error:
+                    error_text += '\n' + error["message"]
+                      for error in errors:
+                self.api_errors.append(error)
+                if "code" in error:
+                    self.api_codes.append(error["code"])
+                if "message" in error:
+                    self.api_messages.append(error["message"])
+                if "code" in error and "message" in error:
+ error_text = ""
+            for error in errors:
+                self.api_errors.append(error)
+                if "code" in error:
+                    self.api_codes.append(error["code"])
+                if "message" in error:
+                    self.api_messages.append(error["message"])
